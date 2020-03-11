a9407b60672764ec5d8fdd21341ce18b5e61de05
dionlenoizap <dionleno.oliveira@izap.com.br>
2020-03-11 18:28:13 -0300

    criando tela de login

---
+++ src/ui/LoginForm/LoginForm.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import './LoginForm.css'
+
+class LoginForm extends Component {
+
+    constructor() {
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+
+        this.state = {
+            email: undefined,
+            password: undefined
+        }
+    }
+
+    static displayName = "ui-LoginForm";
+
+    handleSubmit(e) {
+        e.preventDefault();
+        let dataToSend = {
+            user: {
+                email: this.state.email,
+                password: this.state.password
+            }
+        }
+
+        fetch('http://localhost:3001/users/authenticate', {
+            method: 'POST',
+            body: JSON.stringify(dataToSend),
+             header: {
+                "Content-Type": "application/json"
+            },
+
+        }).then(response => response.json())
+        .then(responseJson => {
+            if(responseJson.sucess){
+                localStorage.setItem('USER_TOKEN', responseJson.token)
+            }
+        })
+    }
+
+    handleEmailChange(e) {
+        this.setState({
+            email: e.target.value
+        })
+    }
+
+    handlePasswordChange(e) {
+        this.setState({
+            password: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div className="wrapper fadeInDown">
+                <div id="formContent">
+
+                    <div className="fadeIn first" style={{ padding: '10px' }}>
+                        <img src="https://image.flaticon.com/icons/svg/1828/1828503.svg" height="100" id="icon" alt="User Icon" />
+                    </div>
+
+                    <form onSubmit={this.handleSubmit}>
+                        <input type="email" onChange={this.handleEmailChange} id="login" className="fadeIn second" name="login" placeholder="login" />
+                        <input type="password" onChange={this.handlePasswordChange} id="password" className="fadeIn third" name="login" placeholder="password" />
+                        <input type="submit" className="fadeIn fourth" value="Log In" />
+                    </form>
+
+                    <div id="formFooter">
+                        <a className="underlineHover" href="#">Forgot Password?</a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default LoginForm;
