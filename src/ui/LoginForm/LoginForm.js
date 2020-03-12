defef8e40cc9c707f9deafa8f28010ef94d744d8
Dionleno <dhonlennon20@gmail.com>
2020-03-12 01:15:12 -0300

    Adicionando tela de cadastro com integração com API

--- src/ui/LoginForm/LoginForm.js
+++ src/ui/LoginForm/LoginForm.js
@@ -6,16 +6,60 @@ class LoginForm extends Component {
     constructor() {
         super();
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleSignUpSubmit = this.handleSignUpSubmit.bind(this);
         this.handleEmailChange = this.handleEmailChange.bind(this);
         this.handlePasswordChange = this.handlePasswordChange.bind(this);
 
         this.state = {
+            name: undefined,
             email: undefined,
-            password: undefined
+            password: undefined,
+            signUp: {
+                success: undefined,
+                message: undefined
+            }
         }
     }
 
     static displayName = "ui-LoginForm";
+
+    handleSignUpSubmit(e) {
+        e.preventDefault();
+        let dataToSend = {
+            user: {
+                name: this.refs.username.value,
+                email: this.refs.email.value,
+                password: this.refs.password.value,
+            }
+        }
+
+        fetch('http://localhost:3003/users/', {
+            method: "POST",
+            body: JSON.stringify(dataToSend.user),
+            headers: {
+                "Content-Type": "application/json"
+            }
+
+        }).then(response => response.json())
+            .then(responseJson => {
+
+                this.setState({
+                    ...this.state,
+                    signUp: {
+                        success: responseJson.success,
+                        message: responseJson.message
+                    }
+                })
+
+            }).catch(err => {
+                throw err;
+            }).finally(
+                this.refs.username.value = "",
+                this.refs.email.value = "",
+                this.refs.password.value = "",
+            );
+
+    }
 
     handleSubmit(e) {
 
@@ -27,7 +71,7 @@ class LoginForm extends Component {
             }
         }
 
-        fetch('http://localhost:3001/users/authenticate', {
+        fetch('http://localhost:3003/users/authenticate', {
             method: "POST",
             body: JSON.stringify(dataToSend.user),
             headers: {
@@ -36,7 +80,11 @@ class LoginForm extends Component {
 
         }).then(response => response.json())
             .then(responseJson => {
-                localStorage.setItem('USER_TOKEN', responseJson.token);
+
+                if (responseJson.success) {
+                    localStorage.setItem('USER_TOKEN', responseJson.token);
+                }
+                alert(responseJson.message);
 
             }).catch(err => {
                 throw err;
@@ -57,24 +105,72 @@ class LoginForm extends Component {
 
     render() {
         return (
-            <div className="wrapper fadeInDown">
-                <div id="formContent">
 
-                    <div className="fadeIn first" style={{ padding: '10px' }}>
-                        <img src="https://image.flaticon.com/icons/svg/1828/1828503.svg" height="100" id="icon" alt="User Icon" />
+            <div>
+
+                {/* Begin Modal Register Form */}
+                <div className="modal fade" id="signupModel" tabIndex="-1" role="dialog" aria-labelledby="signupModelLabel" aria-hidden="true">
+
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="signupModelLabel">Cadastro</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+
+                            <div className="modal-body">
+
+                                {
+                                    this.state.signUp.success !== undefined ? (
+                                        this.state.signUp.success ?
+
+                                            <div className="alert alert-success" role="alert">
+                                                {this.state.signUp.message}
+                                            </div>
+                                            :
+                                            <div className="alert alert-danger" role="alert">
+                                                {this.state.signUp.message}
+                                            </div>
+
+                                    ) : ''
+                                }
+                                <form onSubmit={this.handleSignUpSubmit}>
+                                    <input type="text" id="login" required="required" ref="username" className="fadeIn second" name="login" placeholder="nome" />
+                                    <input type="email" id="login" required="required" ref="email" className="fadeIn second" name="login" placeholder="email" />
+                                    <input type="password" id="password" required="required" ref="password" className="fadeIn third" name="login" placeholder="senha" />
+                                    <input type="submit" className="fadeIn fourth" value="Cadastrar" />
+                                </form>
+                            </div>
+                        </div>
                     </div>
+                </div>
 
-                    <form onSubmit={this.handleSubmit}>
-                        <input type="email" onChange={this.handleEmailChange} id="login" className="fadeIn second" name="login" placeholder="login" />
-                        <input type="password" onChange={this.handlePasswordChange} id="password" className="fadeIn third" name="login" placeholder="password" />
-                        <input type="submit" className="fadeIn fourth" value="Log In" />
-                    </form>
+                {/* Begin Modal Register Form */}
 
-                    <div id="formFooter">
-                        <a className="underlineHover" href="#">Forgot Password?</a>
+                <div className="wrapper fadeInDown">
+                    <div id="formContent">
+
+                        <div className="fadeIn first" style={{ padding: '10px' }}>
+                            <img src="https://image.flaticon.com/icons/svg/1828/1828503.svg" height="100" id="icon" alt="User Icon" />
+                        </div>
+
+                        <form onSubmit={this.handleSubmit}>
+                            <input type="email" required="required" onChange={this.handleEmailChange} id="login" className="fadeIn second" name="login" placeholder="email" />
+                            <input type="password" required="required" onChange={this.handlePasswordChange} id="password" className="fadeIn third" name="login" placeholder="senha" />
+                            <input type="submit" className="fadeIn fourth" value="Entrar" />
+                            <a className="underlineHover" href="#">Esqueceu senha?</a>
+                        </form>
+
+                        <div id="formFooter">
+                            <a className="underlineHover" href="#" data-toggle="modal" data-target="#signupModel">Cadastrar</a>
+                        </div>
                     </div>
                 </div>
             </div>
+
+
         );
     }
 }
